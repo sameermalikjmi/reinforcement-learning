7c86a8abf468cbe7f5a86a3238cd43b87d028c84
Sameer <smalik@wpi.edu>
2022-05-03 01:38:55 -0400

    added PPO implementation for lUnar lander:

---
+++ PPO/MLP.py
@@ -0,0 +1,42 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.distributions as distributions
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout = 0.1):
+        super().__init__()
+        
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.Dropout(dropout),
+            nn.PReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.Dropout(dropout),
+            nn.PReLU(),
+            nn.Linear(hidden_dim, output_dim)
+        )
+        
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
+class ActorCritic(nn.Module):
+    def __init__(self, actor, critic):
+        super().__init__()
+        
+        self.actor = actor
+        self.critic = critic
+        
+    def forward(self, state):
+        
+        action_pred = self.actor(state)
+        value_pred = self.critic(state)
+        
+        return action_pred, value_pred
